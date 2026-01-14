ab2e3fbc06f645aa3a1ffbec7916dd447edbfa58
tomgroos2004-crypto <tomgroos2004@gmail.com>
2026-01-14 13:27:56 +0100

    Update booking.php

--- components/booking.php
+++ components/booking.php
@@ -7,27 +7,25 @@ $embed = $settings['embed'] ?? '';
 ?>
 
 <section class="ls-booking">
-  <div class="ls-section-inner">
-    <div class="ls-container">
+  <div class="ls-container">
 
-      <?php if ($title || $intro) : ?>
-        <header class="ls-booking-header">
-          <?php if ($title) : ?>
-            <h2 class="h2"><?= esc_html($title); ?></h2>
-          <?php endif; ?>
+    <?php if ($title || $intro) : ?>
+      <header class="ls-booking-header">
+        <?php if ($title) : ?>
+          <h2 class="h2"><?= esc_html($title); ?></h2>
+        <?php endif; ?>
 
-          <?php if ($intro) : ?>
-            <p class="lead"><?= esc_html($intro); ?></p>
-          <?php endif; ?>
-        </header>
-      <?php endif; ?>
+        <?php if ($intro) : ?>
+          <p class="lead"><?= esc_html($intro); ?></p>
+        <?php endif; ?>
+      </header>
+    <?php endif; ?>
 
-      <?php if ($embed) : ?>
-        <div class="ls-booking-embed">
-          <?= wp_kses_post($embed); ?>
-        </div>
-      <?php endif; ?>
+    <?php if ($embed) : ?>
+      <div class="ls-booking-embed">
+        <?= wp_kses_post($embed); ?>
+      </div>
+    <?php endif; ?>
 
-    </div>
   </div>
 </section>
