188368d73e2f70b94338da0863879b440653d59c
tomgroos2004-crypto <tomgroos2004@gmail.com>
2026-01-14 13:17:57 +0100

    Update booking.php

--- components/booking.php
+++ components/booking.php
@@ -1,25 +1,31 @@
 <?php
 /**
- * LeadSprint – Booking component
+ * LeadSprint – Booking component (SIMPEL)
  *
  * Verwachte variabelen:
- * - $settings
+ * - $settings['title']
+ * - $settings['intro']
+ * - $settings['embed']
  */
 
-$title  = $settings['title'] ?? '';
-$intro  = $settings['intro'] ?? '';
-$people = $settings['people'] ?? [];
+defined('ABSPATH') || exit;
 
-if (empty($people)) {
+$title = $settings['title'] ?? '';
+$intro = $settings['intro'] ?? '';
+$embed = $settings['embed'] ?? '';
+
+if (!$embed) {
   return;
 }
 
-$uid = wp_unique_id('ls-booking-');
-
+/**
+ * Toegestane embed HTML
+ */
 $allowed = [
   'iframe' => [
     'src'             => true,
     'width'           => true,
+    'height'          => true,
     'frameborder'     => true,
     'allow'           => true,
     'allowfullscreen' => true,
@@ -28,18 +34,9 @@ $allowed = [
     'style'           => true,
   ],
   'div' => [
-    'class'    => true,
-    'style'    => true,
-    'data-url' => true,
-  ],
-  'a' => [
-    'href'   => true,
-    'target' => true,
-    'rel'    => true,
-  ],
-  'p' => [],
-  'span' => [
+    'id'    => true,
     'class' => true,
+    'style' => true,
   ],
   'script' => [
     'src'   => true,
@@ -66,38 +63,8 @@ $allowed = [
         </header>
       <?php endif; ?>
 
-      <div class="ls-booking-tabs" role="tablist" aria-label="<?= esc_attr($title ?: 'Booking'); ?>">
-        <?php foreach ($people as $index => $person) : ?>
-          <?php
-          $name = $person['name'] ?? '';
-          $photo = $person['photo']['url'] ?? '';
-          $input_id = $uid . '-' . $index;
-          $checked = $index === 0 ? 'checked' : '';
-          ?>
-
-          <input
-            class="ls-booking-input"
-            type="radio"
-            name="<?= esc_attr($uid); ?>"
-            id="<?= esc_attr($input_id); ?>"
-            <?= $checked; ?>
-          >
-
-          <label class="ls-booking-tab" for="<?= esc_attr($input_id); ?>">
-            <?php if ($photo) : ?>
-              <span class="ls-booking-avatar">
-                <img src="<?= esc_url($photo); ?>" alt="<?= esc_attr($name); ?>" loading="lazy" decoding="async">
-              </span>
-            <?php endif; ?>
-            <span class="ls-booking-name"><?= esc_html($name); ?></span>
-          </label>
-
-          <div class="ls-booking-content" role="tabpanel">
-			   <div class="ls-booking-embed">
-              <?= wp_kses($person['embed'] ?? '', $allowed); ?>
-            </div>
-          </div>
-        <?php endforeach; ?>
+      <div class="ls-booking-embed">
+        <?= wp_kses($embed, $allowed); ?>
       </div>
 
     </div>
