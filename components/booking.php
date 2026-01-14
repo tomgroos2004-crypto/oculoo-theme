aa450a76e4476032477d6c681b0167ff185d56f1
tomgroos2004-crypto <tomgroos2004@gmail.com>
2026-01-14 13:24:55 +0100

    Update booking.php

--- components/booking.php
+++ components/booking.php
@@ -1,53 +1,12 @@
 <?php
-/**
- * LeadSprint – Booking component (SIMPEL)
- *
- * Verwachte variabelen:
- * - $settings['title']
- * - $settings['intro']
- * - $settings['embed']
- */
-
-defined('ABSPATH') || exit;
+if (!defined('ABSPATH')) exit;
 
 $title = $settings['title'] ?? '';
 $intro = $settings['intro'] ?? '';
 $embed = $settings['embed'] ?? '';
-
-if (!$embed) {
-  return;
-}
-
-/**
- * Toegestane embed HTML
- */
-$allowed = [
-  'iframe' => [
-    'src'             => true,
-    'width'           => true,
-    'height'          => true,
-    'frameborder'     => true,
-    'allow'           => true,
-    'allowfullscreen' => true,
-    'loading'         => true,
-    'title'           => true,
-    'style'           => true,
-  ],
-  'div' => [
-    'id'    => true,
-    'class' => true,
-    'style' => true,
-  ],
-  'script' => [
-    'src'   => true,
-    'async' => true,
-    'type'  => true,
-  ],
-];
 ?>
 
 <section class="ls-booking">
-
   <div class="ls-section-inner">
     <div class="ls-container">
 
@@ -63,11 +22,12 @@ $allowed = [
         </header>
       <?php endif; ?>
 
-      <div class="ls-booking-embed">
-        <?= wp_kses($embed, $allowed); ?>
-      </div>
+      <?php if ($embed) : ?>
+        <div class="ls-booking-embed">
+          <?= wp_kses_post($embed); ?>
+        </div>
+      <?php endif; ?>
 
     </div>
   </div>
-
 </section>
